9ac41c659c9e75d8a693c179eb1c9b2d7e0c351a
Iqra Masroor <masrooriqra@gmail.com>
2020-01-14 18:10:44 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count
+  def initialize (name, artist, genre)
+    @name = name
+    @artisit = artisit
+    @genre = genre
+    @@count= 0
+  end
+end
